99c0dab2b439778aef303ba823219f324b36f3d4
Auii <saintansis@gmail.com>
2022-09-04 00:21:12 +0800

    update readme

--- main/led.c
+++ main/led.c
@@ -10,8 +10,8 @@
 #define LEDC_OUTPUT_IO (3) // Define the output GPIO
 #define LEDC_CHANNEL LEDC_CHANNEL_0
 #define LEDC_DUTY_RES LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
-#define LEDC_DUTY_ON (0)      // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
-#define LEDC_DUTY_OFF (8191)  // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
+#define LEDC_DUTY_ON (0)                // set duty to max
+#define LEDC_DUTY_OFF (8191)            // set duty to min
 #define LEDC_DUTY_DIME (8000) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
 #define LEDC_FREQUENCY (5000) // Frequency in Hertz. Set frequency at 5 kHz
 #define LEDC_FAST_BLINK_INTERVAL (100)
@@ -166,7 +166,8 @@ void ledc_init(void) {
                                         .timer_sel = LEDC_TIMER,
                                         .intr_type = LEDC_INTR_DISABLE,
                                         .gpio_num = LEDC_OUTPUT_IO,
-                                        .duty = LEDC_DUTY_DIME, // Set duty to 0%
+                                        .duty =
+                                            LEDC_DUTY_DIME, // Set duty to 0%
                                         .hpoint = 0};
   ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
   vSemaphoreCreateBinary(led_semaphor);
