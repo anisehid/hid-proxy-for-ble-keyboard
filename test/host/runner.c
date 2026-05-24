1bdd0026578fd63c7adfe4dfcefd006f42e538fb
AnissL93 <saintansis@gmail.com>
2026-05-24 17:58:41 +0100

    Extract pack_ch9329_data as pure function with host tests

--- test/host/runner.c
+++ test/host/runner.c
@@ -3,10 +3,10 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-void run_smoke_tests(void);
+void run_ch9329_pack_tests(void);
 
 int main(void) {
     UNITY_BEGIN();
-    run_smoke_tests();
+    run_ch9329_pack_tests();
     return UNITY_END();
 }
